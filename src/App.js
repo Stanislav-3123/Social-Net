3344950339207a2562975113c5a1f80d6dad6816
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2021-11-10 21:05:18 +0400

    Ruote works

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Content from './components/Content/Content';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
-
+import { Routes, Route, Link } from "react-router-dom";
 
 const App = (props) => {
     return (
@@ -13,10 +13,16 @@ const App = (props) => {
             <Header />
             <Navbar />
             <div className="App-wrapper">
-                <Content />
-                <Dialogs />
+                <Routes>
+                   <Route path="/content" element={<Content />} />
+                   <Route path="/dialogs" element={<Dialogs />} />
+                   <Route path="/dialogs" element={<Dialogs />} />
+                   <Route path="/dialogs" element={<Dialogs />} />
+                   <Route path="/dialogs" element={<Dialogs />} />
+                </Routes>
             </div>
         </div>
+
 
     );
 }
