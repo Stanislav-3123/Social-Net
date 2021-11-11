89df4634c65bb6ef3f230323f42899f1ddc36185
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2021-11-11 15:19:09 +0400

    1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import { Routes, Route, Link } from "react-router-dom";
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 const App = (props) => {
     return (
@@ -16,9 +19,9 @@ const App = (props) => {
                 <Routes>
                    <Route path="/content" element={<Content />} />
                    <Route path="/dialogs" element={<Dialogs />} />
-                   <Route path="/dialogs" element={<Dialogs />} />
-                   <Route path="/dialogs" element={<Dialogs />} />
-                   <Route path="/dialogs" element={<Dialogs />} />
+                   <Route path="/dialogs" element={<Music />} />
+                   <Route path="/dialogs" element={<News />} />
+                   <Route path="/dialogs" element={<Settings />} />
                 </Routes>
             </div>
         </div>
---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import s from './Music.module.css';
+
+
+
+const Music = (props) => {
+    return (
+
+        <div>
+            <div>
+                Albums
+            </div>
+            <div>
+                Music list
+            </div>
+        </div>
+    );
+}
+
+
+export default Music;
