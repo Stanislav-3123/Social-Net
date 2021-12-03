c5cd69c2d32d5b610580a48754cf3e5185f6bccf
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2021-12-03 23:35:34 +0400

    Разделение компоненты Диалоги на подкомпоненты

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
+import Message from './Message/Message';
+import DialogItem from './DialogItem/DialogItem';
 
 
-const DialogItem = (props) => {
-
-    let path = "/dialogs/" + props.id
-
-    return (
-        <div className={s.dialog}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>)
-
-}
-
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-}
 
 const Dialogs = (props) => {
 
