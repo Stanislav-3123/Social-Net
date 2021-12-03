26bbfb2e1278c17550a74ce0349162b567c86b05
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2021-12-03 15:38:11 +0400

    Золотое выделение в навигации

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,19 +11,19 @@ const Navbar = (props) => {
 
         <div className={s.navbar}>
             <div className={s.item}>
-                <NavLink to="/content" activeClassName="active">Profile</NavLink>
+                <NavLink to="/content" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/dialogs" activeClassName={s.active}>Messeges</NavLink>
+                <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messeges</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+                <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+                <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
             </div>
         </div>
 
