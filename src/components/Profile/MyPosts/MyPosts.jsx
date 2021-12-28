f70f936782ca01b4d4669fb4667f0ca149e4304f
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2021-12-29 00:28:49 +0400

    ActionCreator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
+import { addPostActionCreator, 
+    updateNewPostTextActionCreator } from './../../../redux/state'
 
 const MyPosts = (props) => {
 
@@ -13,12 +14,12 @@ const MyPosts = (props) => {
     debugger;
     let addPost = () => {
        // props.addPost();
-       props.dispatch({type: 'ADD-POST'});
+       props.dispatch(addPostActionCreator());
     }
     let onPostChange = () => {
         let text = newPostElement.current.value;
        //  props.updateNewPostText(text);
-       props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text });
+       props.dispatch(updateNewPostTextActionCreator(text));
     }
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+const ADD_POST = 'ADD-POST'
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 let store = {
     _state: {
         messagesPage: {
@@ -74,7 +76,7 @@ let store = {
     // },
 
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 post: this._state.profilePage.newPostText,
@@ -83,7 +85,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -91,6 +93,13 @@ let store = {
     }
 }
 
+export const addPostActionCreator = () => ({type: ADD_POST})
+
+export const updateNewPostTextActionCreator = (text) => ({
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text
+})
+   
 
 // let rerenderEntireTree = () => {
 // console.log ('State is changed!')
