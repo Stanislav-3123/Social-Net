06b0e87b56bccbca2f151379b754e8df33afabd8
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-01-12 22:48:09 +0400

    Вырпвнивание кода

--- src/components/Navbar/Friends/Friends.jsx
+++ src/components/Navbar/Friends/Friends.jsx
@@ -2,15 +2,10 @@ import React from 'react';
 import s from './Friends.module.css';
 import FriendsOnline from './FriendsOnline/FriendsOnline';
 
-
-
-
-
 const Friends = (props) => {
     let friendsOnlineElements =
         props.sidebar.friendsOnline.map(f => <FriendsOnline id={f.id} name={f.name} />
         );
-
     return (
         <div className={s.friendsOnline}>
             <h2 className={s.head}>Friends:</h2>
@@ -20,6 +15,5 @@ const Friends = (props) => {
         </div>
     );
 }
-
 
 export default Friends;
