e580c3540017b9c62eb41f8c146c7203fd9da185
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-01-30 21:55:55 +0400

    устранил дублирование кода

--- src/redux/auth-reduser.js
+++ src/redux/auth-reduser.js
@@ -1,8 +1,8 @@
 import { authAPI } from "../api/api";
-// const SET_USER_DATA = 'SET_USER_DATA'
-const SET_USER_ID = 'SET_USER_ID'
-const SET_USER_EMAIL = 'SET_USER_EMAIL'
-const SET_USER_LOGIN = 'SET_USER_LOGIN'
+const SET_USER_DATA = 'SET_USER_DATA'
+// const SET_USER_ID = 'SET_USER_ID'
+// const SET_USER_EMAIL = 'SET_USER_EMAIL'
+// const SET_USER_LOGIN = 'SET_USER_LOGIN'
 
 let initialState = {
 	login: null,
@@ -14,42 +14,43 @@ let initialState = {
 
 const authReduser = (state = initialState, action) => {
 	switch (action.type) {
-		// case SET_USER_DATA:
-		//     return {
-		//         ...state, 
-		//         ...action.data,
-		//         isAuth: true
-		//     }
-		case SET_USER_ID:
+		case SET_USER_DATA:
 			return {
 				...state,
-				id: action.id,
+				...action.data,
 				isAuth: true
 			}
-		case SET_USER_EMAIL:
-			return {
-				...state,
-				email: action.email,
-			}
-		case SET_USER_LOGIN:
-			return {
-				...state,
-				login: action.login
-			}
+		// case SET_USER_ID:
+		// 	return {
+		// 		...state,
+		// 		id: action.id,
+		// 		isAuth: true
+		// 	}
+		// case SET_USER_EMAIL:
+		// 	return {
+		// 		...state,
+		// 		email: action.email,
+		// 	}
+		// case SET_USER_LOGIN:
+		// 	return {
+		// 		...state,
+		// 		login: action.login
+		// 	}
 		default:
 			return state;
 	}
 }
-// export const setAuthUserData = (userId, email, login) => ({type:SET_USER_DATA, data: {userId, email, login}});
-export const setAuthUserId = (id) => ({ type: SET_USER_ID, id });
-export const setAuthUserEmail = (email) => ({ type: SET_USER_EMAIL, email });
-export const setAuthUserLogin = (login) => ({ type: SET_USER_LOGIN, login });
+export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: { userId, email, login } });
+// export const setAuthUserId = (id) => ({ type: SET_USER_ID, id });
+// export const setAuthUserEmail = (email) => ({ type: SET_USER_EMAIL, email });
+// export const setAuthUserLogin = (login) => ({ type: SET_USER_LOGIN, login });
 export const getAuthUserData = () => (dispatch) => {
 	authAPI.me().then(response => {
 		if (response.data.resultCode === 0) {
-			dispatch(setAuthUserId(response.data.data.id));
-			dispatch(setAuthUserEmail(response.data.data.email));
-			dispatch(setAuthUserLogin(response.data.data.login));
+			dispatch(setAuthUserData(response.data.data.id));
+			// dispatch(setAuthUserId(response.data.data.id));
+			// dispatch(setAuthUserEmail(response.data.data.email));
+			// dispatch(setAuthUserLogin(response.data.data.login));
 		}
 	});
 }
