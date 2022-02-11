1662bdce26e9a75063e7bec77918af9b700a35d4
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-02-11 22:37:26 +0400

    useState

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,42 +2,42 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
-const MyPosts = (props) => {
+const MyPosts = React.memo(props => {
+	console.log("RENDER");
+	let myPostsElements =
+		[...props.profilePage.posts]
+			.reverse()
+			.map(p => <Post message={p.post} likecounts={p.likecounts} />);
+	let newPostElement = React.createRef();
 
-
-    let myPostsElements =
-        props.profilePage.posts.map(p => <Post message={p.post} likecounts={p.likecounts} />);
-    let newPostElement = React.createRef();
-
-    let onAddPost = () => {
-        props.addPost();
-    }
-    let onPostChange = () => {
-        let text = newPostElement.current.value;
-        props.updateNewPostText(text);
-    }
-    return (
-        <div className={s.itemAll}>
-            <div className={s.item}>
-                <h3>My Posts</h3>
-            </div>
-            <div className={s.item}>
-                <div>
-                    <textarea
-                        onChange={onPostChange}
-                        ref={newPostElement}
-                        placeholder="New post"
-                        value={props.profilePage.newPostText}
-                    />
-                </div>
-                <button onClick={onAddPost}>Add post</button>
-            </div>
-            <div className={s.itemPosts}>
-                {myPostsElements}
-            </div>
-        </div>
-    );
-}
-
+	let onAddPost = () => {
+		props.addPost();
+	}
+	let onPostChange = () => {
+		let text = newPostElement.current.value;
+		props.updateNewPostText(text);
+	}
+	return (
+		<div className={s.itemAll}>
+			<div className={s.item}>
+				<h3>My Posts</h3>
+			</div>
+			<div className={s.item}>
+				<div>
+					<textarea
+						onChange={onPostChange}
+						ref={newPostElement}
+						placeholder="New post"
+						value={props.profilePage.newPostText}
+					/>
+				</div>
+				<button onClick={onAddPost}>Add post</button>
+			</div>
+			<div className={s.itemPosts}>
+				{myPostsElements}
+			</div>
+		</div>
+	);
+});
 
 export default MyPosts;
