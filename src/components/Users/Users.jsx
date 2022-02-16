e5e2f5225b353ac5d1dc13fd951ac11382bf4593
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-02-16 22:04:01 +0400

    Paginator + библиотека Classnames

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import style from './Users.module.css';
-import PaginatorTest from '../../common/Paginator/PaginatorTest';
+import Paginator from '../../common/Paginator/Paginator';
 import User from './User';
 
 let Users = ({ currentPage, onPageChange, totalUsersCount, pageSize, users, followingInProgress, unfollow, follow, ...props }) => {
 	return <div className={style.wrapper}>
-		<PaginatorTest currentPage={currentPage} onPageChange={onPageChange}
-			totalUsersCount={totalUsersCount} pageSize={pageSize} />
-		<div>{users.map(u => <User key={u.id}
+		<Paginator currentPage={currentPage} onPageChange={onPageChange}
+			totalItemsCount={totalUsersCount} pageSize={pageSize} />
+		<div className={style.userItems}>{users.map(u => <User key={u.id}
 			user={u}
 			followingInProgress={followingInProgress}
 			follow={follow}
