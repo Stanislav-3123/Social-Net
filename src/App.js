4e83b5ef261abd6b4cdf57aa9a7ee1e31c767df2
Stanislav-3123 <91882335+Stanislav-3123@users.noreply.github.com>
2022-02-17 17:41:49 +0400

    React.Lazy

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import './App.css';
-// import Profile from './components/Profile/Profile';
+import React, { Suspense } from 'react';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Music from './components/Music/Music';
 import News from './components/News/News';
 import Settings from './components/Settings/Settings';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+// import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContaier from './components/Users/UsersContaier';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import LoginPage from './components/Login/Login';
+import Preloader from './common/preloader/preloader';
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
 const App = () => {
 	return (
-		<div className="App" >
-			<HeaderContainer />
-			<Navbar />
-			<div className="App-wrapper">
-				<Routes>
-					<Route path="/profile/*"
-						element={<ProfileContainer />} />
-					<Route path="/users/*" element={<UsersContaier />} />
-					<Route path="/dialogs/*"
-						element={<DialogsContainer />} />
-					<Route path="/music/*" element={<Music />} />
-					<Route path="/news/*" element={<News />} />
-					<Route path="/settings/*" element={<Settings />} />
-					<Route path="/login/*" element={<LoginPage />} />
-				</Routes>
+		<Suspense fallback={<Preloader />}>
+			<div className="App" >
+				<HeaderContainer />
+				<Navbar />
+				<div className="App-wrapper">
+					<Routes>
+						<Route path="/profile/*"
+							element={<ProfileContainer />} />
+						<Route path="/users/*" element={<UsersContaier />} />
+						<Route path="/dialogs/*"
+							element={<DialogsContainer />} />
+						<Route path="/music/*" element={<Music />} />
+						<Route path="/news/*" element={<News />} />
+						<Route path="/settings/*" element={<Settings />} />
+						<Route path="/login/*" element={<LoginPage />} />
+					</Routes>
+				</div>
 			</div>
-		</div>
+		</Suspense>
 	);
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,7 +3,6 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = React.memo(props => {
-	console.log("RENDER");
 	let myPostsElements =
 		[...props.profilePage.posts]
 			.reverse()
